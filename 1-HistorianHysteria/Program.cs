359096dd7f804960d125d7f98add895a8cbc3de4
Daniel Revell <mail@danielrevell.com>
2024-12-04 23:36:02 +0000

    First exercise and a half

---
+++ 1-HistorianHysteria/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> left = [];
+List<int> right = [];
+List<int> distances = [];
+List<int> similarities = [];
+
+foreach (var line in File.ReadAllLines("real-input.txt"))
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    left.Add(int.Parse(parts[0]));
+    right.Add(int.Parse(parts[1]));
+}
+
+left = left.Order().ToList();
+right = right.Order().ToList();
+
+for (var i = 0; i < left.Count; i++)
+{
+    distances.Add(Math.Abs(left[i] - right[i]));
+}
+
+var totalDistance = distances.Sum();
+
+Console.WriteLine("The total distance is {0}", totalDistance);
+
+for (var i = 0; i < left.Count; i++)
+{
+    var number = left[i];
+    var occurrences = right.Count(c => c == number);
+    similarities.Add(number * occurrences);
+}
+
+var similarityScore = similarities.Sum();
+
+Console.WriteLine("The similarity score is {0}", similarityScore);
---
+++ 2-RedNosedReports/Program.cs
@@ -0,0 +1,35 @@
+﻿var safeReports = File.ReadAllLines("test-input.txt")
+    .Select(report => report.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToList())
+    .Count(AreLevelsSafe);
+
+Console.WriteLine("There are {0} safe reports", safeReports);
+return;
+
+bool AreLevelsSafe(IList<int> levels)
+{
+    var startingDirection = Math.Sign(levels[0] - levels[1]);
+    var strikes = 0;
+    
+    for (var i = 0; i < levels.Count - 1; i++)
+    {
+        var delta = levels[i] - levels[i + 1];
+        
+        if (Math.Sign(delta) != startingDirection || Math.Abs(delta) is < 1 or > 3)
+        {
+            if (--strikes >= 0)
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    return strikes >= 0;
+
+    //return true;
+}
