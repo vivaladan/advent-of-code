6c0f1c387e09e448ac54f3976546ae8c2c9181a7
Daniel Revell <=>
2025-12-02 16:53:54 +0000

    day 1

---
+++ 2025/AdventOfCode2025.Tests/Day1Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2025.Tests;
+
+public class Day1Tests
+{
+    private static string ProjectDir => Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
+    private static string[] LoadLinesFromFile(string filename) =>
+        File.ReadAllLines(Path.Combine(ProjectDir, filename))
+            .Where(c => !string.IsNullOrWhiteSpace(c))
+            .Select(c => c.Trim())
+            .ToArray();
+
+    [Fact]
+    public void Day1_Part1_Sample()
+    {
+        Assert.Equal("3", Day1.GetPwdFromSeq_Part1(["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]));
+    }
+
+    [Fact]
+    public void Day1_Part1_Input()
+    {
+        Assert.Equal("1195", Day1.GetPwdFromSeq_Part1(LoadLinesFromFile("Day1Input.txt")));
+    }
+
+    [Fact]
+    public void Day1_Part2_Sample()
+    {
+        Assert.Equal("6", Day1.GetPwdFromSeq_Part2(["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]));
+    }
+
+    [Fact]
+    public void Day1_Part2_Input()
+    {
+        Assert.Equal("6770", Day1.GetPwdFromSeq_Part2(LoadLinesFromFile("Day1Input.txt")));
+    }
+}
---
+++ 2025/AdventOfCode2025/Day1.cs
@@ -0,0 +1,124 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2025;
+
+public static class Day1
+{
+    public static string GetPwdFromSeq_Part1(string[] seq)
+    {
+        var dial = 50;
+        var zeroCount = 0;
+
+        Debug.WriteLine($"The dial starts by pointing at 50.");
+
+        foreach (var rot in seq)
+        {
+            var val = int.Parse(rot[1..]);
+
+            val %= 100;
+
+            switch (rot[0])
+            {
+                case 'L':
+                {
+                    var dec = dial - val;
+                    if (dec < 0)
+                    {
+                        dial = dec + 100;
+                    }
+                    else
+                    {
+                        dial = dec;
+                    }
+
+                    break;
+                }
+                case 'R':
+                {
+                    var inc = dial + val;
+                    if (inc > 99)
+                    {
+                        dial = inc - 100;
+                    }
+                    else
+                    {
+                        dial = inc;
+                    }
+
+                    break;
+                }
+            }
+
+            Debug.WriteLine($"The dial is rotated {rot} to point at {dial}");
+
+            if (dial == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+        return zeroCount.ToString();
+    }
+
+    public static string GetPwdFromSeq_Part2(string[] seq)
+    {
+        var dial = 50;
+        var zeroCount = 0;
+
+        foreach (var rot in seq)
+        {
+            var clicks = int.Parse(rot[1..]);
+
+            // Each full rotation past 0 counts as an additional zero
+            zeroCount += clicks / 100;
+
+            // Only the remainder affects the dial position
+            clicks %= 100;
+
+            switch (rot[0])
+            {
+                case 'L':
+                {
+                    var left = dial - clicks;
+                    if (left < 0)
+                    {
+                        // When prev rotation ends at 0, we have already counted that zero
+                        if (dial != 0) zeroCount++;
+                    
+                        // Wrap around
+                        dial = left + 100;
+                    }
+                    else
+                    {
+                        dial = left;
+                        if (dial == 0) zeroCount++;
+                    }
+
+                    break;
+                }
+                case 'R':
+                {
+                    var right = dial + clicks;
+                    if (right > 99)
+                    {
+                        // When prev rotation ends at 0, we have already counted that zero
+                        if (dial != 0) zeroCount++;
+
+                        // Wrap around
+                        dial = right - 100;
+                    }
+                    else
+                    {
+                        dial = right;
+                        if (dial == 0) zeroCount++;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return zeroCount.ToString();
+    }
+
+}
